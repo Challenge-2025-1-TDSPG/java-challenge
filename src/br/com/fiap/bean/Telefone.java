bc0f2e8291b2d2b29ae11cc66be41c92d46d8e79
alex-isidro <alexander.d.dennis@gmail.com>
2025-05-14 19:51:27 -0300

    add classe telefone

---
+++ src/br/com/fiap/bean/Telefone.java
@@ -0,0 +1,14 @@
+package br.com.fiap.bean;
+
+public class Telefone {
+    //atributos
+    private String telefone;
+
+    //construtores
+    public Telefone() {
+    }
+
+    public Telefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
