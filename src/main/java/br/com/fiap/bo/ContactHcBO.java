90dcd6a45bcacce062f24e1ee58ce3dc2d667c46
alex-isidro <alexander.d.isidro@gmail.com>
2025-10-15 23:34:01 -0300

    feat(ContactHcBO): criação do ContactHcBO

---
+++ src/main/java/br/com/fiap/bo/ContactHcBO.java
@@ -0,0 +1,30 @@
+package br.com.fiap.bo;
+
+import br.com.fiap.dao.ContactHcDAO;
+import br.com.fiap.to.ContactHcTO;
+
+import java.util.ArrayList;
+
+public class ContactHcBO {
+    private ContactHcDAO contactDAO;
+
+    public ArrayList<ContactHcTO> findAll() {
+        contactDAO = new ContactHcDAO();
+        return contactDAO.findAll();
+    }
+
+    public ContactHcTO findById(Long id) {
+        contactDAO = new ContactHcDAO();
+        return contactDAO.findById(id);
+    }
+
+    public ContactHcTO save(ContactHcTO contact) {
+        contactDAO = new ContactHcDAO();
+        return contactDAO.save(contact);
+    }
+
+    public boolean delete(Long id) {
+        contactDAO = new ContactHcDAO();
+        return contactDAO.delete(id);
+    }
+}
